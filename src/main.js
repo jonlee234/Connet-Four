8d33929314d5819dcf203c0ff963a3e0fee150ad
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-06-30 13:20:09 -0400

    Can now add discs to columns

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 const map =
- ["EEEEEEE",
- "EEEEEEE",
- "EEEEEEE",
- "EEEEEEE",
- "EEEEEEE",
- "EEEEEEE",
- 
-]
+    ["EEEEEEE",
+        "EEEEEEE",
+        "EEEEEEE",
+        "EEEEEEE",
+        "EEEEEEE",
+        "EEEEEEE",
 
-let x=5
-let y=7
+    ]
+
+let x = 5
+let y = 7
 
 
 
@@ -26,11 +26,11 @@ const createBoard = function (blueprint) {
         let blockDivs = ''
         for (let colNum = 0; colNum < rowString.length; colNum++) {
             const blockType = rowString[colNum]
-           
+
 
             if (blockType === 'E') {
-                blockDivs += `<div class ="disc" data-x = "${rowNum}"data-y = "${colNum}" data></div> `
-               
+                blockDivs += `<div class ="disc" data-x = "${rowNum}"data-y = "${colNum}"></div> `
+
             }
 
 
@@ -42,13 +42,45 @@ const createBoard = function (blueprint) {
 }
 
 createBoard(map);
-
-
 let discs = document.querySelectorAll(".disc")
 
-function addDisc(event){
+
+function findLastEmptySpot(positionOfY) {
+    for (let i = 0; i < discs.length; i++) {
+        if (discs[i].dataset.y === positionOfY) {
+            for (let k = 0; k < 6; k++) {
+                console.log(discs[i].classList.contains("player1"))
+                if (discs[i].classList.contains("player1") === false && discs[i].classList.contains("player2") === false) {
+                    console.log(discs[i])
+                    return discs[i]
+                }
+
+            }
+        }
+    }
+    return console.log("no empty spaces")
+
+
+
+}
+
+// console.log(discs)
+let playerCounter = 1
+function addDisc(event) {
     console.log(event.target)
-    // for(let i = )
+    if (playerCounter % 2 === 1) {
+        let yPosition = event.target.dataset.y
+        let emptySpot = findLastEmptySpot(yPosition)
+        emptySpot.classList.add("player1")
+        playerCounter++
+    }
+    else if (playerCounter % 2 === 0) {
+        let yPosition = event.target.dataset.y
+        let emptySpot = findLastEmptySpot(yPosition)
+        emptySpot.classList.add("player2")
+        playerCounter++
+
+    }
 }
 
 
